0bc613f6a00dcc795c38f2b813115c8900e152c1
Harun Shaban <42933471+harunshaban@users.noreply.github.com>
2020-03-19 22:49:45 -0700

    Created base model controller

---
+++ MultiLanguageApplication/MyBaseController.cs
@@ -0,0 +1,39 @@
+﻿using MultiLanguageApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultiLanguageApplication
+{
+    public class MyBaseController : Controller
+    {
+        protected override IAsyncResult BeginExecuteCore(AsyncCallback callback, object state)
+        {
+            string lang = null;
+            HttpCookie langCookie = Request.Cookies["culture"];
+            if(langCookie != null)
+            {
+                lang = langCookie.Value;
+            }
+            else
+            {
+                var userLanguage = Request.UserLanguages;
+                var userLang = userLanguage != null ? userLanguage[0] : "";
+                if(userLang != "")
+                {
+                    lang = userLang;
+                }
+                else
+                {
+                    lang = SiteLanguage.getDefaultLanguage();
+                }
+            }
+
+            new SiteLanguage().setLanguage(lang);
+
+            return base.BeginExecuteCore(callback, state);
+        }
+    }
+}
